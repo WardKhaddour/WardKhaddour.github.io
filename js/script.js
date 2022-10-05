cb2b8ec7485b4b25abdd9de37cfa9e6054b8b501
WardKhaddour <wardkhaddot@gmail.com>
2022-10-05 16:57:11 +0300

    fix observer

--- js/script.js
+++ js/script.js
@@ -8,18 +8,21 @@ import nav from './nav.js';
 import about from './data/about.js';
 import { observer, colorNavItem } from './observer.js';
 const container = document.querySelector('.container');
+const mainContent = document.querySelector('.main');
 // const themeSelector = document.querySelector('.color-theme__selector');
 
 container.insertAdjacentHTML('beforebegin', nav);
 const navList = document.querySelector('.nav__list');
 
-container.innerHTML += header(
+container.innerHTML = `${header(
   "Hello, I'am",
   about.name,
   about.job,
   about.summary,
   about.img
-);
+)} ${container.innerHTML}`;
+
+const headerItem = document.querySelector('.header');
 
 navList.addEventListener('click', e => {
   if (e.target.tagName !== 'A') return;
@@ -33,6 +36,6 @@ function navScroll(el) {
     .scrollIntoView({ behavior: 'smooth' });
 }
 
-[...container.children].forEach(child => {
+[...mainContent.children, headerItem].forEach(child => {
   observer.observe(child);
 });
