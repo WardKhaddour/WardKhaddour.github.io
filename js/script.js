a9cd7f523cf3bf40643f4d7d29a2e5823bb865ca
WardKhaddour <wardkhaddot@gmail.com>
2022-10-05 03:16:16 +0300

    fix variables

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 import header from './header.js';
+import nav from './nav.js';
 import about from './data/about.js';
 
+// const body = document.querySelector('body');
 const container = document.querySelector('.container');
 
+// container.insertAdjacentHTML('beforebegin', nav);
 container.innerHTML += header(
   "Hello, I'am",
   about.name,
