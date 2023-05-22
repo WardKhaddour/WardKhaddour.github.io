ca4cb8cb70e309d3f19fb3f78bfeaa6ae9426f74
WardKhaddour <wardkhaddot@gmail.com>
2023-05-22 22:07:55 +0300

    update projects

--- js/data/projects.js
+++ js/data/projects.js
@@ -1,13 +1,23 @@
 export default [
   {
     id: 1,
-    imgs: [`/img/7-1.png`, '/img/7-2.png', '/img/7-3.png', '/img/7-4.png'],
-    name: 'Learning System',
+    imgs: [`/img/9-1.png`],
+    name: 'Topic Nexus',
     description:
-      'Learning System Landing page with complete Authentication System, Built with Vue.js & Quasar (NOT completed)',
+      'Social Media application built with React, Redux toolkit and Typescript',
+    liveLink: 'https://topic-nexus.vercel.app',
+    codeLink: 'https://github.com/WardKhaddour/TopicNexus',
   },
   {
     id: 2,
+    imgs: [`/img/10-1.png`],
+    name: 'Protected Routes with React',
+    description:
+      'This project is about routing with React and React Router V6 and how to protect routes',
+    codeLink: 'https://github.com/WardKhaddour/React-Protected-Routes',
+  },
+  {
+    id: 3,
     imgs: [`/img/1-1.png`, '/img/1-2.png'],
     name: 'Tic Tac Toe',
     description:
@@ -16,15 +26,15 @@ export default [
     codeLink: 'https://github.com/WardKhaddour/Tic-Tac-Toe',
   },
   {
-    id: 3,
-    imgs: [`/img/6-1.png`],
-    name: 'Online Shop',
-    description:
-      'Convert The online shop app front-end from server side rendering with EJS templates to separate Vue.js App (NOT completed)',
-    codeLink: 'https://github.com/WardKhaddour/Online-Shop-frontend',
-  },
-  {
     id: 4,
+    imgs: [`/img/7-1.png`, '/img/7-2.png', '/img/7-3.png', '/img/7-4.png'],
+    name: 'Learning System',
+    description:
+      'Learning System Landing page with complete Authentication System, Built with Vue.js & Quasar (NOT completed)',
+  },
+
+  {
+    id: 5,
     imgs: [`/img/8-1.png`, `/img/8-2.png`],
     name: 'Simple landing page',
     description: `Created a simple landing page with about, products, contact and docs sections. 
@@ -34,7 +44,16 @@ export default [
     liveLink: 'https://wardkhaddour.github.io/Landing-Page/',
   },
   {
-    id: 5,
+    id: 6,
+    imgs: [`/img/6-1.png`],
+    name: 'Online Shop',
+    description:
+      'Convert The online shop app front-end from server side rendering with EJS templates to separate Vue.js App (NOT completed)',
+    codeLink: 'https://github.com/WardKhaddour/Online-Shop-frontend',
+  },
+
+  {
+    id: 7,
     imgs: [`/img/5-1.png`],
     name: 'Natours',
     description:
@@ -43,7 +62,7 @@ export default [
       'https://github.com/WardKhaddour/Learning-node-js/tree/main/Natours',
   },
   {
-    id: 6,
+    id: 8,
     imgs: [`/img/2-1.png`, '/img/2-2.png'],
     name: 'Landing page for Natours',
     description: `Built with Advanced CSS and SASS.
@@ -52,7 +71,7 @@ export default [
     codeLink: 'https://github.com/WardKhaddour/NATOURS-Advanced-CSS-Course',
   },
   {
-    id: 7,
+    id: 9,
     imgs: [`/img/3-1.png`],
     name: 'Nexter',
     description: `Built with Advanced CSS and SASS.
@@ -61,7 +80,7 @@ export default [
     codeLink: 'https://github.com/WardKhaddour/NEXTER-Advanced-CSS-Course',
   },
   {
-    id: 8,
+    id: 10,
     imgs: [`/img/4-1.png`],
     name: 'Trillo',
     description: `Built with Advanced CSS and SASS.
